3bca23cd779f17b5ae14ca61025c7a7d017a5e36
rosyid22 <72512051+rosyid22@users.noreply.github.com>
2020-12-11 22:35:39 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var n = prompt("selamat datang di website ponorgo");
+var n = prompt("selamat datang di website kesenian reog ponorgo");
 var c = confirm("Hello " + n + " selamat membaca ");
 if (c == true) {
     alert('enjoy');
