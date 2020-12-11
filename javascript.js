bf7b0b5562c8770fc4ea72beff436939c847e37b
rosyid22 <72512051+rosyid22@users.noreply.github.com>
2020-12-11 22:06:26 +0700

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var n = prompt("selamat datang di website ponorgo");
+var c = confirm("Hello" + n + " selamat membaca ");
+if (c == true) {
+    alert('enjoy');
+} else {
+    alert('No problem');
+}
